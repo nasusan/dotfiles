03aea948c4e03856aa395223c855f658c3da0060
nasusan <watanabexy@gmail.com>
2017-08-26 18:00:23 +0900

    [add] setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
